cdbf4ea16a5419ce22fd721f77413176f512c24c
Benoit Barthelet <benoit.barthelet@gmail.com>
2013-11-22 14:55:19 +0100

    changed to mydotfiles

--- makesymlinks.sh
+++ makesymlinks.sh
@@ -6,8 +6,8 @@
 
 ########## Variables
 
-dir=~/dotfiles                    # dotfiles directory
-olddir=~/dotfiles_old             # old dotfiles backup directory
+dir=~/mydotfiles                    # dotfiles directory
+olddir=~/mydotfiles_old             # old dotfiles backup directory
 files="bashrc vimrc vim"    # list of files/folders to symlink in homedir
 
 ##########
